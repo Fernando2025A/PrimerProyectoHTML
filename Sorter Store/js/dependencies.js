3104767ad79264be288888dd5c6b83c783a59c7b
Fernando2025A <fernandoezequielgarcia2008@gmail.com>
2025-06-23 22:18:35 -0300

    Add files via upload

---
+++ Sorter Store/js/dependencies.js
@@ -0,0 +1,5 @@
+
+let dinero = 0;
+document.getElementById("dinero").textContent = "$" + dinero;
+
+let maxRecarga = 20;
--- Sorter Store/js/functions.js
+++ Sorter Store/js/functions.js
@@ -2,3 +2,34 @@ function HideAll()
 {
    document.getElementById("pr").style.display = "none";
 }
+
+function Recargar()
+{
+   let dineroRecargado = prompt("¿Cuánto quiere recargar? (Puede recargar hasta $20, mínimo $5 por recarga)");
+   
+
+   if (dineroRecargado != null)
+   {
+      if (dineroRecargado >= 5)
+      {
+         if (dineroRecargado <= maxRecarga)
+         {
+            dinero+=parseInt(dineroRecargado);
+           document.getElementById("dinero").textContent = "$" + dinero;
+           maxRecarga-=dineroRecargado;
+         }
+         else
+         {
+            alert("El máximo que aún puedes recargar es de: " + maxRecarga);
+         }
+      }
+      else
+      {
+        alert("¡El monto mínimo es de $5!");
+      }
+   }
+   else
+   {
+      alert("Campo vacío");
+   }
+}
